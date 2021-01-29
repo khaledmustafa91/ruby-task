7cd272da8d0eb29b4e7131bba2d66b783ce62d15
Khaled Mustafa <km282596@gmail.com>
2021-01-29 20:13:52 +0200

    make unit test for all requests with happy and unhappy scenarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :user, only: [:create,:index,:destroy]
+  resources :user, only: [:create,:destroy]
   resources :post do
     resources :comment
   end
